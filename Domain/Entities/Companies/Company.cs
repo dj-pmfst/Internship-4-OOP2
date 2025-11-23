744a866386e7019f882ea70a11ba8ae3d42223b5
dj-pmfst <djuric@pmfst.hr>
2025-11-23 20:06:17 +0100

    Initial commit-skeleton

---
+++ Domain/Entities/Companies/Company.cs
@@ -0,0 +1,16 @@
+﻿namespace Domain.Entities.Company
+{
+    internal class Company
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public async Task Create()
+        {
+
+        }
+        public async Task CreateOrUpdateValidation()
+        {
+            if (Name?.Length > 150) { } //triba bit unique
+        }
+    }
+}
---
+++ Domain/Entities/Users/User.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Entities.Users
+{
+    internal class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Username { get; set; }
+        public DateOnly DoB {  get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string adressStreet { get; set; }
+        public string adressCity { get; set; }
+        public decimal geoLat { get; set; }
+        public decimal geoLng { get; set; }
+        public string? website { get; set; }
+        public TimestampAttribute createdAt { get; set; }
+        public TimestampAttribute updatedAt { get; set; }
+        public bool isActive { get; set; } = true;
+        public async Task Create()
+        {
+
+        }
+        public async Task CreateOrUpdateValidation()
+        {
+            if (Name?.Length > 100) { }
+            if (Surname?.Length > 100) { }
+            if (adressStreet?.Length > 150) { }
+            if (adressCity?.Length > 150) { }
+            if (website?.Length > 100) { } //valid url pattern 
+
+        } 
+
+    }
+}
